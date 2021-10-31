a728ef73c30fa69343beaa96d3803065d188fe37
Пехов Алексей Владимирович <Pekhov.A.V@sberbank.ru>
2021-10-31 11:53:46 +0300

    fix

--- src/test/java/ru/praktikum/yandex/QuestionsTest.java
+++ src/test/java/ru/praktikum/yandex/QuestionsTest.java
@@ -1,5 +1,7 @@
 package ru.praktikum.yandex;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +11,8 @@ import ru.praktikum.yandex.pages.mainpage.MainPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.text.IsEmptyString.isEmptyString;
 
 public class QuestionsTest {
 
@@ -18,6 +20,7 @@ public class QuestionsTest {
 
     @BeforeEach
     public void setUp() {
+        Configuration.holdBrowserOpen = true;
         mainPage = open(MainPage.URL, MainPage.class);
     }
 
@@ -26,7 +29,7 @@ public class QuestionsTest {
         ElementsCollection questions = mainPage.getQuestionsList();
         for(SelenideElement question : questions) {
             question.scrollIntoView(true).click();
-            assertThat(question.$(By.className("accordion__panel")).getText(), not(isEmptyString()));
+            assertThat(question.$(By.className("accordion__panel")).shouldBe(Condition.visible).getText(), not(isEmptyString()));
         }
     }
 }
